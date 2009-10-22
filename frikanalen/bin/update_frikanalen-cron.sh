39218a105cef68efa64df6828b19ca0d3e9be107
Petter Reinholdtsen <pere@hungry.com>
2009-10-22 08:53:42 +0000

    Minor cleanup. update_frikanalen-cron.sh

--- frikanalen/bin/update_frikanalen-cron.sh
+++ frikanalen/bin/update_frikanalen-cron.sh
@@ -1,19 +1,19 @@
 #!/usr/local/bin/bash
 
 typeset -i c=0
-bindir="/data/video/frikanalen/bin/"
-htmldir="/data/video/frikanalen/"
+bindir="/data/video/frikanalen/bin"
+htmldir="/data/video/frikanalen"
 
 echo "###### `date` #####"
 
 echo "Updating meta.xml" 
-> ${htmldir}meta.xml
+> $htmldir/meta.xml
 
 while  [[ true ]]; do 
  c=$c+1
  echo "Take $c"
- ${bindir}update_meta_xml.pl
- if [[ $(wc -l ${htmldir}meta.xml|awk '{print $1}') -lt 50 ]]
+ $bindir/update_meta_xml.pl
+ if [[ $(wc -l $htmldir/meta.xml|awk '{print $1}') -lt 50 ]]
  then
   sleep 30
  else
@@ -21,7 +21,4 @@ while  [[ true ]]; do
  fi
 done
 
-${bindir}/update_video_diff.pl
-
-
-
+$bindir/update_video_diff.pl

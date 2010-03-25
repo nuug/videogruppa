b45d68c43d99a59a43b89b6f2a9dd1bdbdfbfbd1
Petter Reinholdtsen <pere@hungry.com>
2010-03-25 20:33:25 +0000

    Convert to bourne shell.

--- frikanalen/bin/update_frikanalen-cron.sh
+++ frikanalen/bin/update_frikanalen-cron.sh
@@ -1,6 +1,5 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
-typeset -i c=0
 bindir="/data/video/frikanalen/bin"
 htmldir="/data/video/frikanalen"
 
@@ -9,12 +8,14 @@ echo "###### `date` #####"
 echo "Updating meta.xml" 
 > $htmldir/meta.xml
 
-while  [[ true ]]; do 
- c=$c+1
+c=0
+while true ; do 
+ c=$(expr $c + 1 )
  echo "Take $c"
- $bindir/update_meta_xml.pl
- if [[ $(wc -l $htmldir/meta.xml|awk '{print $1}') -lt 50 ]]
- then
+ $bindir/update_meta_xml.pl $htmldir/meta.xml
+
+ # Verify a successfull download
+ if [ "$(wc -l < $htmldir/meta.xml)" -lt 50 ]; then
   sleep 30
  else
   break

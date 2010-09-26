c3afe9890c68544441cec376b1b62a864d052b66
Petter Reinholdtsen <pere@hungry.com>
2010-09-26 08:37:05 +0000

    Typo.

--- tools/gen_frikanalen_video.pl
+++ tools/gen_frikanalen_video.pl
@@ -154,7 +154,7 @@ sub create_startposter_png {
  my $name = shift;
  my $title_lines = break_title($meta->{'title'});
  my $bgfile = shift;
- my $f = "convert $bgfile -pointsize 72 -fill white -gravity NorthWest -draw "text 450,167 \'$meta->{'presenter'}:\'" -pointsize 60 -draw "text 450,300 \'$title_lines->[0]\'" -draw "text 450,380 \'$title_lines->[1]\'" -draw "text 450,460 \'$title_lines->[2]\'" -draw "text 450,540 \'$title_lines->[3]\'" -pointsize 36 -pointsize 36 -draw "text 52,790 \'$meta->{'url'}\'" -draw "text 750,640 \'$meta->{'place'}: $meta->{'date'}\'" $name";
+ my $f = "convert $bgfile -pointsize 72 -fill white -gravity NorthWest -draw \"text 450,167 \'$meta->{'presenter'}:\'\" -pointsize 60 -draw \"text 450,300 \'$title_lines->[0]\'\" -draw \"text 450,380 \'$title_lines->[1]\'\" -draw \"text 450,460 \'$title_lines->[2]\'\" -draw \"text 450,540 \'$title_lines->[3]\'\" -pointsize 36 -pointsize 36 -draw \"text 52,790 \'$meta->{'url'}\'\" -draw \"text 750,640 \'$meta->{'place'}: $meta->{'date'}\'\" $name";
  if ( !runcmd($f) ) { die "Failed to execute system command in" . (caller(0))[3] ."\n"; }
 }
 
@@ -191,7 +191,7 @@ sub create_endposter_png {
  }
  my $name = shift;
  my $bgfile = shift;
- my $f = "convert $bgfile -pointsize $text_size -fill white -gravity NorthWest $cmd_body -pointsize 36 -draw "text 52,790 \'$meta->{'url'}\'" -draw "text 750,640 \'$meta->{'place'}: $meta->{'date'}\'" $name";
+ my $f = "convert $bgfile -pointsize $text_size -fill white -gravity NorthWest $cmd_body -pointsize 36 -draw \"text 52,790 \'$meta->{'url'}\'\" -draw \"text 750,640 \'$meta->{'place'}: $meta->{'date'}\'\" $name";
  if ( !runcmd($f) ) { die "Failed to execute system command in" . (caller(0))[3] ."\n"; }
 }
 

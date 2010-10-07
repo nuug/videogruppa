278cde191401fdb1377e77296fe5f32024f547d2
Jarle Bjørgeengen <jarle@bjorgeengen.net>
2010-10-07 08:34:40 +0000

    Old stuff missed

--- frikanalen/bin/cron_update_stream_videos.sh
+++ frikanalen/bin/cron_update_stream_videos.sh
@@ -5,6 +5,6 @@ workdir="/home/jarle/svn/frikanalen-bin"
 cd ${workdir}
 if  [[ `pwd` == ${workdir} ]]
 then
-find . -name 'broadcast*.ogv' -mtime +1 -exec rm {} \;
+find . -name 'broadcast*.ogv' -atime +1 -exec rm {} \;
 ./scheduler -g -o  > download.log 2>&1 & 
 fi
